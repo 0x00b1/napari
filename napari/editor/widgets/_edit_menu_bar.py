89430cd9cff0763d956cebd842e1d851b7127a46
Allen Goodman <allen.goodman@icloud.com>
2020-06-04 14:20:43 -0400

    controllers

--- napari/editor/widgets/_edit_menu_bar.py
+++ napari/editor/widgets/_edit_menu_bar.py
@@ -9,63 +9,74 @@ class EditMenuBar(QMenuBar):
 
         self.edit_menu = edit_menu_bar.addMenu('&Edit')
 
-        self.edit_menu.addAction(QAction(
-            parent=self,
-            shortcut='Ctrl+Z',
-            statusTip="Undo last operation",
-            text='&Undo',
-            triggered=self.editor.on_undo,
-        ))
+        self.edit_menu.addAction(
+            QAction(
+                parent=self,
+                shortcut='Ctrl+Z',
+                statusTip="Undo last operation",
+                text='&Undo',
+                triggered=self.editor.on_undo,
+            )
+        )
 
-        self.edit_menu.addAction(QAction(
-            parent=self,
-            shortcut='Ctrl+Shift+Z',
-            statusTip="Redo last operation",
-            text='&Redo',
-            triggered=self.editor.on_redo,
-        ))
+        self.edit_menu.addAction(
+            QAction(
+                parent=self,
+                shortcut='Ctrl+Shift+Z',
+                statusTip="Redo last operation",
+                text='&Redo',
+                triggered=self.editor.on_redo,
+            )
+        )
 
         self.addSeparator()
 
         self.addAction()
 
-        self.edit_menu.addAction(QAction(
-            parent=self,
-            shortcut='Ctrl+X',
-            statusTip="Cut to clipboard",
-            text='Cu&t',
-            triggered=self.editor.on_cut,
-        ))
+        self.edit_menu.addAction(
+            QAction(
+                parent=self,
+                shortcut='Ctrl+X',
+                statusTip="Cut to clipboard",
+                text='Cu&t',
+                triggered=self.editor.on_cut,
+            )
+        )
 
         self.addAction()
 
-        self.edit_menu.addAction(QAction(
-            parent=self,
-            shortcut='Ctrl+C',
-            statusTip="Copy to clipboard",
-            text='&Copy',
-            triggered=self.editor.on_copy,
-        ))
+        self.edit_menu.addAction(
+            QAction(
+                parent=self,
+                shortcut='Ctrl+C',
+                statusTip="Copy to clipboard",
+                text='&Copy',
+                triggered=self.editor.on_copy,
+            )
+        )
 
         self.addAction()
 
-        self.edit_menu.addAction(QAction(
-            parent=self,
-            shortcut='Ctrl+V',
-            statusTip="Paste from clipboard",
-            text='&Paste',
-            triggered=self.editor.on_paste,
-        ))
+        self.edit_menu.addAction(
+            QAction(
+                parent=self,
+                shortcut='Ctrl+V',
+                statusTip="Paste from clipboard",
+                text='&Paste',
+                triggered=self.editor.on_paste,
+            )
+        )
 
         self.addAction()
 
-        self.edit_menu.addAction(QAction(
-            parent=self,
-            shortcut='Del',
-            statusTip="Delete selected items",
-            text='&Delete',
-            triggered=self.editor.on_delete,
-        ))
+        self.edit_menu.addAction(
+            QAction(
+                parent=self,
+                shortcut='Del',
+                statusTip="Delete selected items",
+                text='&Delete',
+                triggered=self.editor.on_delete,
+            )
+        )
 
         self.addAction()
-
