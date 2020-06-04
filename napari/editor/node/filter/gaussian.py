27d78043390d67ac0494b5c6209facbdaa347379
Allen Goodman <allen.goodman@icloud.com>
2020-06-04 13:45:38 -0400

    settings

---
+++ napari/editor/node/filter/gaussian.py
@@ -0,0 +1,34 @@
+from skimage.filters import gaussian
+
+from ..node import Node
+from ...setting.integer_setting import IntegerSetting
+from ...socket.layer.image_socket import ImageSocket
+from ....layers import Image
+
+
+class Gaussian(Node):
+    def __init__(self):
+        super().__init__("Gaussian")
+
+        self.sigma = IntegerSetting("Radius", default=1)
+
+        self.image = ImageSocket("Image", self.id)
+
+        self.response = ImageSocket("Response", self.id)
+
+    @property
+    def options(self) -> ['Setting']:
+        return [self.sigma]
+
+    @property
+    def x(self) -> ['Socket']:
+        return [self.image]
+
+    @property
+    def y(self) -> ['Socket']:
+        return [self.response]
+
+    def evaluate(self):
+        data = gaussian(self.image.data, sigma=self.sigma.data)
+
+        self.response.data = Image(data)
